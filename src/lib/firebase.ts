a7bf50c9bd5046b222a66ebf8fee4febd9176723
Marcos Vinicius Fagundes Müller <marcosm7m@hotmail.com>
2025-09-23 00:40:29 +0000

    vou publicar o codigo na vercel, quais as variaveis de ambiente que prec

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,12 +7,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "studio-6667769576-2f158",
-  "appId": "1:925902748075:web:dbb1ff7cea55227b077aee",
-  "apiKey": "AIzaSyBSc5SwYqo_Fubz_6giEouk537tdZfOSFg",
-  "authDomain": "studio-6667769576-2f158.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "925902748075"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
