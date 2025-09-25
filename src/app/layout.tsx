83794becd0a9df7d92aa853dcc2f5f6d92a55698
Marcos Vinicius Fagundes Müller <marcosm7m@hotmail.com>
2025-09-25 23:18:18 +0000

    gere o manifest e se possivel crie o splash screen do tema para o pwa e

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@400;500;600;700&display=swap" rel="stylesheet" />
-        <link rel="icon" href="/icons/icon-48x48.png" sizes="any" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
       </head>
       <body className={cn('font-body antialiased')}>
         <AuthProvider>
