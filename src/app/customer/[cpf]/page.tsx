b82408e8705cea6e212593fa91e7aee33b140b67
Marcos Vinicius Fagundes Müller <marcosm7m@hotmail.com>
2025-09-25 23:01:56 +0000

    Console Error
    A param property was accessed directly with `params.cpf`

--- src/app/customer/[cpf]/page.tsx
+++ src/app/customer/[cpf]/page.tsx
@@ -3,18 +3,13 @@
 import { useEffect } from 'react';
 import SalesDashboard from '@/components/sales-dashboard';
 import { useAuth } from '@/components/auth-provider';
-import { useRouter } from 'next/navigation';
+import { useRouter, useParams } from 'next/navigation';
 import { Loader2 } from 'lucide-react';
 
-type PageProps = {
-  params: {
-    cpf: string;
-  };
-};
-
-export default function CustomerPage({ params }: PageProps) {
+export default function CustomerPage() {
   const { user, loading } = useAuth();
   const router = useRouter();
+  const params = useParams();
 
   useEffect(() => {
     if (!loading && !user) {
@@ -30,7 +25,9 @@ export default function CustomerPage({ params }: PageProps) {
     );
   }
 
-  const customerCpf = decodeURIComponent(params.cpf);
+  const customerCpf = Array.isArray(params.cpf)
+    ? decodeURIComponent(params.cpf[0])
+    : decodeURIComponent(params.cpf as string);
 
   return (
     <main className="container mx-auto p-4 md:p-8">
